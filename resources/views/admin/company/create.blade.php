74b42a2557f59cd640d4023e75b3ea99594fd2a2
Alex <alexxalexx7488@gmail.com>
2020-12-07 21:06:21 +0500

    fixed

--- resources/views/admin/company/create.blade.php
+++ resources/views/admin/company/create.blade.php
@@ -18,7 +18,7 @@
             <div class="row justify-content-center">
                 <div class="col-md-9">
                     <div class="card">
-                        <div class="card-header">Созидай давай</div>
+                        <div class="card-header">Создание</div>
                         <div class="card-body">
 
                             <div class="form-group row">
